d9b4053d1538589e321ad8b66305e556a7469e13
tomaszlacina <tomasz@migo.money>
2021-09-18 10:08:48 +0200

    zadanie 1 i 2  z walidaji

---
+++ src/main/java/pl/coderslab/app/controller/BookValidationController.java
@@ -0,0 +1,39 @@
+package pl.coderslab.app.controller;
+
+
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pl.coderslab.app.entity.Book;
+
+@Controller
+@RequestMapping("/validation")
+@RequiredArgsConstructor
+public class BookValidationController {
+
+  private final Validator validator;
+
+  @GetMapping("/book")
+  @ResponseBody
+  public String validateBook(){
+    Book book = new Book();
+
+    Set<ConstraintViolation<Book>> constraintViolations = validator.validate(book);
+
+    StringBuilder sb = new StringBuilder();
+
+    for (ConstraintViolation violation : constraintViolations){
+      sb.append(violation.getPropertyPath());
+      sb.append("   :   ");
+      sb.append(violation.getMessage());
+      sb.append("<br/>");
+    }
+
+    return sb.toString();
+  }
+}
--- src/main/java/pl/coderslab/app/entity/Book.java
+++ src/main/java/pl/coderslab/app/entity/Book.java
@@ -7,7 +7,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
 @Entity
 @Table(name = "books")
@@ -18,12 +22,19 @@ public class Book {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Size(min = 5)
   private String title;
 
+  @Range(min = 1, max = 10)
   private int rating;
 
+  @Size(max = 600)
   private String description;
 
   @ManyToOne
+  @NotNull
   private Publisher publisher;
+
+  @Min(1)
+  private int pages;
 }
